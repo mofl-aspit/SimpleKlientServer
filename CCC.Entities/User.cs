f578f28d437a88a189d82c0d2ac1a80a84add4d7
mort225g <mort225g@edu.campusvejle.dk>
2016-10-06 10:54:52 +0200

    startet working on entities, finished logger

---
+++ CCC.Entities/User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CCC.Entities
+{
+    public class User
+    {
+        #region Constants
+
+        #endregion
+
+        #region Fields
+        private string username;
+        private string password;
+
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Properties
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                if (value.Length > 20)
+                {
+                    throw new ArgumentException("the name is too long");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("the name can't be empty");
+                }
+                else
+                {
+                    username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                if (value.Length > 30)
+                {
+                    throw new ArgumentException("the Password is too long");
+                }
+                else if (value.Length < 8)
+                {
+                    throw new ArgumentException("your Password is too short");
+                }
+                else if(Regex.IsMatch(value, @"^-?\d+$"))
+                {
+                    throw new ArgumentException("Password contains unauthorized characters");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("the Password can't be empty");
+
+                }
+                else
+                {
+                    password = value;
+                }
+
+            }
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
--- Server.DataAccess/Logger.cs
+++ Server.DataAccess/Logger.cs
@@ -33,8 +33,18 @@
         /// </summary>
         static Logger()
         {
-            //finds the file path and binds it to the "path"
-            path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + TextDotTxt;
+            try
+            {
+                //finds the file path and binds it to the "path"
+                path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + TextDotTxt;
+            }
+            //AE = Argument Exeption
+            catch(ArgumentException AE)  { throw AE; }
+            //PE = Path Exeption
+            catch(PathTooLongException PE) { throw PE; }
+            //NE = Not Exeption
+            catch(NotSupportedException NE) { throw NE; }
+            
         }
         #endregion
         
@@ -43,18 +53,34 @@
         #endregion
 
         #region Methods
+        /// <summary>
+        /// method for creating a log or writing to an existing log
+        /// </summary>
+        /// <param name="message">the log message(string)</param>
         public static void Log(string message)
         {
-
-            // checks if there is a file like the one in path, 
-            // if yes then it overwrites it, if no then it creates a new one
-            StreamWriter file = new StreamWriter(path, append: true);
-            //writes a line equal to string message
-            
-            file.WriteLine($"{DateTime.Now.ToString(DateFormat)}" + Space + message);
-            //closes the file for safety and stuff
-            file.Close();
-
+            try
+            {
+                // checks if there is a file like the one in path, 
+                // if yes then it overwrites it, if no then it creates a new one
+                StreamWriter file = new StreamWriter(path, append: true);
+                //writes a line equal to string message
+                file.WriteLine($"{DateTime.Now.ToString(DateFormat)}" + Space + message);
+                //closes the file for safety and stuff
+                file.Close();
+            }
+            //UE = Unathorized Exeption
+            catch(UnauthorizedAccessException UE) { throw UE; }
+            //AE = ArgumentExeption
+            catch(ArgumentException AE) { throw AE; }
+            //DE = Directory Exeption
+            catch(DirectoryNotFoundException DE) { throw DE; }
+            //IE = I Exeption
+            catch(IOException IE) { throw IE; }
+            //OE = Object Exeption
+            catch(ObjectDisposedException OE) { throw OE; }
+            //FE = Format Exeption
+            catch(FormatException FE) { throw FE; }
         }
         #endregion
     }
