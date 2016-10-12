adfa350f1c58ae91eb41d178f2bdd4170191a64e
mort225g <mort225g@edu.campusvejle.dk>
2016-10-12 10:16:04 +0200

    added a Database with a table to contain a name and a password

--- Client.Controllers/Class1.cs
+++ Client.Controllers/Class1.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CCC.Entities;
+﻿
 
 namespace Client.Controllers
 {
+    #region Usings
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CCC.Entities;
+    #endregion
     public class Class1
     {
     }
---
+++ UserDB/UserTable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[UserTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL
+)
